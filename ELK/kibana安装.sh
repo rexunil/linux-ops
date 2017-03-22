146ba2c5ab8176b3285e0c2b5c14d208ce267113
rexunil <yue232@163.com>
2017-03-22 11:11:02 +0800

    Update kibana安装.sh

--- ELK/kibana安装.sh
+++ ELK/kibana安装.sh
@@ -4,9 +4,9 @@
 
 rpm --import https://artifacts.elastic.co/GPG-KEY-elasticsearch
 
-cat >> /etc/yum.repos.d/logstash.repo <<EOF
-[logstash-5.x]
-name=Elastic repository for 5.x packages
+cat >> /etc/yum.repos.d/kibana.repo <<EOF
+[kibana-5.x]
+name=Kibana repository for 5.x packages
 baseurl=https://artifacts.elastic.co/packages/5.x/yum
 gpgcheck=1
 gpgkey=https://artifacts.elastic.co/GPG-KEY-elasticsearch
@@ -15,13 +15,15 @@ autorefresh=1
 type=rpm-md
 EOF
 
-yum install -y logstash
+yum install -y kibana
 
-cp /etc/logstash/logstash.yml{,.original}
+cp /etc/kibana/kibana.yml{,.original}
 
 systemctl daemon-reload
 systemctl enable kibana.service
 systemctl start kibana.service
 
 # http://localhost:5601
+
+
 

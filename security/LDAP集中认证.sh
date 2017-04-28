ed479e3466771809baa45041af7ffe536377f0f1
rexunil <yue232@163.com>
2017-04-28 15:46:29 +0800

    Update LDAP集中认证.sh

--- security/LDAP集中认证.sh
+++ security/LDAP集中认证.sh
@@ -1,2 +1,7 @@
 2017.4.28搭了一遍。除了用户目录挂载没做之外其他的都做了。
 可以给每个人生成一个账号呀，可以登录所有服务器，其操作登陆都可以审计。
+
+加上sudo权限细分，应该是无敌了。
+## Allow root to run any commands anywhere 
+root    ALL=(ALL)       ALL
+ops1    ALL=(ALL)       ALL
